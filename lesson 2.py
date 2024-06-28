3783eb323f1d601cd65ef71612ffb6d66ea21612
Mik-user <mikmikh@yandex.ru>
2024-06-28 10:07:25 +0300

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,8 @@
+# Практическое занятие "СТРОКИ И ИНДЕКСАЦИЯ СТРОК"
+example='навуходоносор'
+print (example[0])
+print (example[-1])
+a=len(example)
+print(example[int(a/2):a])
+print (example[::-1])
+print (example[1:a:2])
